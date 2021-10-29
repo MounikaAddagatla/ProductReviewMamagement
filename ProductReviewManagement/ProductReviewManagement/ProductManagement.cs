294c914b063c88b0c9f24c9676d7e5e9f3a6e397
unknown <mounaadagatla@gmail.com>
2021-10-29 12:03:04 +0530

    Add : UC4 for count no of ratings by id

--- ProductReviewManagement/ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductReviewManagement/ProductManagement.cs
@@ -18,7 +18,18 @@ namespace ProductReviewManagement
             foreach (var list in recordData)
             {
                 Console.WriteLine("ProductID : " + list.ProductID + " UserID : " + list.UserID + " Rating : " + list.Rating
-                    + " Review : " + list.Review + " isLike : " + list.IsLike);
+                    + " Review : " + list.Review + " IsLike : " + list.IsLike);
+            }
+        }
+        public void SelectIdRecord(List<ProductReviewModel> listProductReview)
+        {
+            var recordData = from ProductReview in listProductReview
+                             where (ProductReview.ProductID == 3 || ProductReview.ProductID == 9 || ProductReview.ProductID == 6) && ProductReview.Rating > 5
+                             select ProductReview;
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID : " + list.ProductID + " UserID : " + list.UserID + " Rating : " + list.Rating
+                    + " Review : " + list.Review + " IsLike : " + list.IsLike);
             }
         }
     }
